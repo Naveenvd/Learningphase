8610bee88aa2b3f2b1eafb69f0cd1213f8a10182
Naveen Vijayakumar <151433395+Naveenvd@users.noreply.github.com>
2025-04-22 15:12:36 +0530

    Time: 62 ms (5.09%) | Memory: 46.5 MB (82.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -8,14 +8,31 @@ class Solution {
             map.put(i,map.getOrDefault(i,0)+1);
 
         }
-        List<Map.Entry<Integer,Integer>> list=new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.<Integer,Integer>comparingByValue().reversed());
-        
-        
+        // List<Map.Entry<Integer,Integer>> list=new ArrayList<>(map.entrySet());
+        // list.sort(Map.Entry.<Integer,Integer>comparingByValue().reversed());
+  
+        // int[] arr=new int[k];
+        // for(int i=0;i<k;i++)
+        // {
+        //     arr[i]=list.get(i).getKey();
+        // }
+        // return arr;
         int[] arr=new int[k];
         for(int i=0;i<k;i++)
         {
-            arr[i]=list.get(i).getKey();
+            int max=0;
+            int key=0;
+            for(int n:map.keySet())
+            {
+                if(map.get(n)>max)
+                {
+                    max=map.get(n);
+                    key=n;
+                }
+               
+            }
+            arr[i]=key;
+            map.remove(key);
         }
         return arr;
         
