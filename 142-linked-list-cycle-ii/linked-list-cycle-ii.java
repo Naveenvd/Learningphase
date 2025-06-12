fefc6a800bb7a612d7b14be2878759f5c28ba8e0
Naveen Vijayakumar <151433395+Naveenvd@users.noreply.github.com>
2025-06-12 12:10:13 +0530

    Time: 4 ms (8.67%) | Memory: 45.2 MB (5.76%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> visited = new HashSet<>();
+        while (head != null) {
+            if (visited.contains(head)) {
+                return head; 
+            }
+            visited.add(head);
+            head = head.next;
+        }
+        return null; 
+    }
+}
