f5ca6695a3d0d0f96e218ebc7d099a112fcf778d
Naveen Vijayakumar <151433395+Naveenvd@users.noreply.github.com>
2025-06-14 12:04:17 +0530

    Time: 5 ms (2.89%) | Memory: 44.5 MB (5.68%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -18,34 +18,39 @@ class Solution {
             map.put(ptr.val,map.getOrDefault(ptr.val,0)+1);
             ptr=ptr.next;
         }
-        ArrayList<Integer> list=new ArrayList<>();
+        ListNode dummy=new ListNode(0);
+        ListNode h=dummy;
+        // ArrayList<Integer> list=new ArrayList<>();
         for(Map.Entry<Integer,Integer> entry:map.entrySet())
         {
             if(entry.getValue()==1)
             {
-                list.add(entry.getKey());
-              
+                // list.add(entry.getKey());
+                ListNode newnode=new ListNode(entry.getKey());
+                dummy.next=newnode;
+                dummy=newnode;
             }
         }
-        head=arr_ll(list);
-        return head;
+        // head=arr_ll(list);
+        // return head;
+        return h.next;
 
 
     }
-    public static ListNode arr_ll(ArrayList<Integer> list)
-    {
-        if(list.size()==0)
-        {
-            return null;
-        }
-        ListNode head=new ListNode(list.get(0));
-        ListNode temp=head;
-        for(int i=1;i<list.size();i++)
-        {
-            ListNode newnode=new ListNode(list.get(i));
-            temp.next=newnode;
-            temp=newnode;
-        }
-        return head;
-    }
+    // public static ListNode arr_ll(ArrayList<Integer> list)
+    // {
+    //     if(list.size()==0)
+    //     {
+    //         return null;
+    //     }
+    //     ListNode head=new ListNode(list.get(0));
+    //     ListNode temp=head;
+    //     for(int i=1;i<list.size();i++)
+    //     {
+    //         ListNode newnode=new ListNode(list.get(i));
+    //         temp.next=newnode;
+    //         temp=newnode;
+    //     }
+    //     return head;
+    // }
 }
